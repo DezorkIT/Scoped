94f843b2a7a060ca4d70232760c13b48ccbd3560
DezorkIT <dezorkdev@gmail.com>
2022-08-13 20:27:37 +0300

    added JD

--- Sources/Scoped/Scoped.swift
+++ Sources/Scoped/Scoped.swift
@@ -2,6 +2,16 @@ public protocol Scoped {}
 
 public extension Scoped where Self: Any {
 
+    /// Allows you to set properties on an instance with a closure
+    ///
+    ///     let adam = Person("adam")
+    ///     .apply {
+    ///        $0.age = 32
+    ///        $0.city = "London"
+    ///     }
+    ///     print(adam)
+    ///
+    /// - NOTE: it copies value types and pointers
     @inlinable
     func apply(_ block: (inout Self) throws -> Void) rethrows -> Self {
        var copy = self
@@ -9,14 +19,29 @@ public extension Scoped where Self: Any {
        return copy
     }
 
-    @inlinable
-    func `let`<R>(_ block: (Self) throws -> R) rethrows -> R {
-        try block(self)
-    }
-
+    /// Allows you to set properties on an instance with a closure. Doesn't copy anything.
+    ///
+    ///     let imageView = UIImageView().also {
+    ///     $0.image = UIImage(systemName: "iphone")
+    ///     $0.contentMode = .scaleAspectFill
+    ///     }
     @inlinable
     func also(_ block: (Self) throws -> Void) rethrows -> Self {
         try block(self)
         return self
+    }
+
+    /// Allows you to execute something with a closure and returns it's result
+    ///
+    ///     let numbers = ["one", "two", "three", "four"]
+    ///     let modifiedFirstItem = numbers.first.let { firstItem in
+    ///        guard let firstItem = firstItem else { return "" }
+    ///        print("The first item of the list is \(firstItem)")
+    ///        return firstItem.count >= 5 ? firstItem : "!" + firstItem + "!"
+    ///     }.uppercased()
+    ///     print("First item after modifications: \(modifiedFirstItem)")
+    @inlinable
+    func `let`<R>(_ block: (Self) throws -> R) rethrows -> R {
+        try block(self)
     }
 }
