dc85f43f21a70ea94f3df010b2e8a350c0fe6874
DezorkIT <dezorkdev@gmail.com>
2022-08-16 20:50:00 +0300

    added new conformances

--- Sources/Scoped/Scoped+Extensions.swift
+++ Sources/Scoped/Scoped+Extensions.swift
@@ -1,4 +1,13 @@
 import Foundation
 
 extension Optional: Scoped {}
+
+// MARK: - NS extensions
 extension NSObject: Scoped {}
+
+// MARK: CG extensions
+extension CGPoint: Scoped {}
+extension CGRect: Scoped {}
+extension CGSize: Scoped {}
+extension CGVector: Scoped {}
+extension CGAffineTransform: Scoped {}
